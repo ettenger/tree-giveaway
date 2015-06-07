0bc1ee81894dbf91eddaf2056dcc866b1ce8d2a6
Andrew Ettenger <ettenger@gmail.com>
2015-06-07 14:52:00 -0400

    Validates presence of name and email for request.

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,9 @@
 class Request < ActiveRecord::Base
   belongs_to :tree
   belongs_to :giveaway
+
+  validates :first_name, :last_name, presence: true
+  validates :tree_id, presence: { message: "Please select a tree" }, numericality: { only_integer: true }
+  validates :email, uniqueness: true, presence: true
+
 end
