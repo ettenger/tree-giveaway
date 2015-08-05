52dcb7b8173fc2deb42dfc52006f1a7dbeab0da8
Andrew Ettenger <ettenger@gmail.com>
2015-08-05 16:25:05 -0400

    Added client side validation for email address and planting city

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -9,7 +9,7 @@ class RequestsController < ApplicationController
     @request = Request.new(request_params_with_tree_and_addy)
 
     if Request.where(giveaway_id: @request.giveaway_id).map(&:planting_address).include? @request.planting_address
-      redirect_to Giveaway.find(request_params[:giveaway_id]), notice: "Tree not reserved. You may only request one tree per planting address."
+      redirect_to Giveaway.find(request_params[:giveaway_id]), notice: "Tree not reserved. You may only make one request per planting address."
       return
     end
       
--- app/models/request.rb
+++ app/models/request.rb
@@ -2,7 +2,7 @@ class Request < ActiveRecord::Base
   belongs_to :tree
   belongs_to :giveaway
 
-  validates :first_name, :last_name, :phone_number,
+  validates :first_name, :last_name, :phone_number, :referral,
             :mailing_street1, :mailing_city, :mailing_state, :mailing_zip,
             :planting_street1, :planting_city, :planting_state, :planting_zip,
             presence: true
