b8d0c72c66f1eb05afc11043d5aae713ed84067f
Andrew Ettenger <ettenger@gmail.com>
2016-02-13 13:04:21 -0500

    Update logo administrative pages

--- app/models/logo.rb
+++ app/models/logo.rb
@@ -1,4 +1,7 @@
 class Logo < ActiveRecord::Base
+  belongs_to :giveaway
+
   has_attached_file :image
+  validates :name, presence: true
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
