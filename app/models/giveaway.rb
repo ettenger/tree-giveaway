c01032ebf60309d465c2ab8c9dd10f1809b4a7e4
Andrew Ettenger <ettenger@gmail.com>
2017-08-21 20:33:36 -0400

    More helpers for one-time links

--- app/models/giveaway.rb
+++ app/models/giveaway.rb
@@ -52,6 +52,7 @@ class Giveaway < ActiveRecord::Base
     self.used_codes.split(",").include? user_code
   end
 
+  # don't forget to save after using
   def use_code!(user_code)
     self.valid_codes = self.valid_codes_arr.reject { |code| code == user_code }.join(",")
     
@@ -64,6 +65,19 @@ class Giveaway < ActiveRecord::Base
 
   def valid_one_time_links
     self.valid_codes_arr.map { |code| "#{self.url}?code=#{code}"}
+  end
+
+  def generate_random_codes(num)
+    codes = []
+    num.times do |i|
+      codes << ('a'..'z').to_a.shuffle[0,8].join
+    end
+    codes
+  end
+
+  # don't forget to save after using
+  def set_random_codes!
+    self.valid_codes = self.generate_random_codes(100).join(",")
   end
 
 end
