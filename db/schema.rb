aa455436ae663ef3dc8a0d2deb5ab7938921f548
Andrew Ettenger <ettenger@gmail.com>
2018-01-25 23:23:09 -0500

    Client side validation for radio buttons

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180126033640) do
+ActiveRecord::Schema.define(version: 20180126040751) do
 
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string   "data_file_name",               null: false
